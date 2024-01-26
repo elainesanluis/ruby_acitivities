e46f5097eed121bb15348a74b71bcab1678968f3
root <root@whereise>
2024-01-26 22:20:55 +0800

    updated poly inheritance

---
+++ poly_inherit.rb
@@ -0,0 +1,21 @@
+class Ninja
+    attr_reader :name, :rank
+  
+    def initialize(name, rank)
+      @name = name
+      @rank = rank
+    end
+  
+    def train
+      puts "#{@name} trains hard to improve their skills."
+    end
+  end
+  
+  class RasenganUser < Ninja
+    def can_use_rasengan?
+      true
+    end
+  end
+  
+  naruto = RasenganUser.new("Naruto Uzumaki", "Hokage")
+  puts naruto.can_use_rasengan? 
