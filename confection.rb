94b7c3c891faddb8206dc5911983519a0fab1282
root <root@whereise>
2024-01-26 22:47:35 +0800

    updated confection file

---
+++ confection.rb
@@ -0,0 +1,33 @@
+class Confection
+    def initialize(name)
+      @name = name
+    end
+  
+    def prepare
+      puts "#{@name}: Baking at 350 degrees for 25 minutes."
+    end
+  end
+  
+  class Cupcake < Confection
+    def initialize(name, frosting_type)
+      super(name)
+      @frosting_type = frosting_type
+    end
+  
+    def prepare
+      super
+      puts "#{@name}: Applying #{@frosting_type} frosting."
+    end
+  end
+  
+  class BananaCake < Confection
+  end
+  
+  # Example usage
+  cupcake = Cupcake.new("Cupcake", "Pistachio")
+  cupcake.prepare
+  
+  
+  banana_cake = BananaCake.new("Banana Cake")
+  banana_cake.prepare
+  
