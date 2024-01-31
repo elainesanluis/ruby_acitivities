5b9f9a7e9c8e9d8b99c9d294b85d084e87b6f5f4
root <root@whereise>
2024-02-01 00:04:07 +0800

    added alternative answer using loop

--- Isograms/isograms.rb
+++ Isograms/isograms.rb
@@ -1,11 +1,26 @@
-def is_isogram(str)
-    lowercase_str = str.downcase
-    lowercase_str.each_char do |char|
-      return false if lowercase_str.count(char) > 1
+# Using each method
+
+def is_isogram(x)
+    check = x.downcase
+    check.each_char do |char|
+      return false if check.count(char) > 1
     end
     true
   end
-  
-  puts is_isogram("cat")  
-  puts is_isogram("mama")
-  puts is_isogram("tanjiro") 
+
+# Using loop method
+  def is_isogram(x)
+    check = x.downcase
+    for i in 0...x.length
+        for j in (i + 1)...x.length
+            if check[i] == check[j]
+                return false
+            end
+          end
+        end
+        return true
+        end
+
+puts is_isogram("cat")  
+puts is_isogram("mama")
+puts is_isogram("tanjiro") 
