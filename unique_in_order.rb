76ce526943c90d146fa83752aa135e8cd22203c4
root <root@whereise>
2024-02-01 20:24:56 +0800

    added .inspect to code

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -15,8 +15,8 @@ def unique_in_order(x)
   end
 
 #Test the method
-puts unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-puts unique_in_order('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
-puts unique_in_order([1,2,2,3,3]) == [1,2,3]
+puts unique_in_order('AAAABBBCCDAABBB').inspect
+puts unique_in_order('ABBCcAD').inspect
+puts unique_in_order([1,2,2,3,3]).inspect
 
   
